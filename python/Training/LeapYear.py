41724dc88a1b067594e90dfb26d84676ab3845cf
Reza Aghajani <rezaaghajani@protonmail.com>
2020-12-22 15:50:47 +0330

    Leap Year Checker in Python ✔

---
+++ python/Training/LeapYear.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+year = int(input("Enter your year: "))
+
+if year % 4 == 0:
+    if year % 100 ==0:
+        if year % 400 == 0:
+            print("The year " + str(year) + " is Leap year")
+else:
+    print("The year {year} is NOT Leap year")
