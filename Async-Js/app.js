ac76172ede08249f21c6d1a6d4e805961c58df3d
Reza Aghajani <rezaaghajani@protonmail.com>
2021-01-31 10:09:20 +0330

    Example of Sync Code

--- Async-Js/app.js
+++ Async-Js/app.js
@@ -1,4 +1,11 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then(response => response.json())
-    .then(json => console.log(json))
+// sync code example
 
+function test() {
+    console.log('hey')
+}
+
+console.log('Start')
+
+test()
+
+console.log('End')
