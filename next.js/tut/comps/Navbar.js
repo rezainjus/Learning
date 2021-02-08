87e5cd4edf9b19b507fd54c2fd04a5a7fa495a45
Reza Aghajani <rezaaghajani@protonmail.com>
2021-02-08 14:52:01 +0330

    Working with components in next.js

---
+++ next.js/tut/comps/Navbar.js
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return ( 
+        <div>
+            <nav>
+                <div className="logo">
+                    <h1>Reza Aghajani</h1>
+                </div>
+
+                <a href="#">Home</a>
+                <a href="/about">About</a>
+                <a href="/lists">Lists</a>
+            </nav>
+        </div>
+     );
+}
+ 
+export default Navbar;
--- next.js/tut/pages/index.js
+++ next.js/tut/pages/index.js
@@ -1,10 +1,16 @@
 import Head from 'next/head'
+import Footer from '../comps/Footer'
+import Navbar from '../comps/Navbar'
 import styles from '../styles/Home.module.css'
+
 
 export default function Home() {
   return (  
     <div>
+      <Navbar />
       <h1>Homepage</h1>
+      <Footer />
     </div>
+
   )
 }
