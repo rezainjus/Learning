93631f403eee8f0109c4ac4afb7f61aa1fe4bbf7
Reza Aghajani <rezaaghajani@protonmail.com>
2021-02-08 15:11:03 +0330

    Work with styles and style module in Next.js

--- next.js/tut/pages/index.js
+++ next.js/tut/pages/index.js
@@ -8,9 +8,11 @@ import Link from 'next/link'
 export default function Home() {
   return (  
     <div>
-      <h1>Homepage</h1>
-      <Link href="/lists">
-        <a> See our lists</a>
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque minima deleniti voluptas, ex consequuntur earum, tempora veniam rerum voluptatibus provident, labore aspernatur nulla qui distinctio voluptatum exercitationem quaerat. Voluptatibus, obcaecati!</p>
+      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ipsum sed omnis, sequi consequatur, voluptates quae eos architecto saepe aspernatur quidem eligendi at aliquid iure facere qui dolore temporibus harum?</p>
+      <Link href="/lists" >
+        <a className={styles.btn}> See our lists</a>
       </Link>
     </div>
 
