7f1394a44a80a8d288b3d01b8842ad129c7f8e95
Reza Aghajani <rezaaghajani@protonmail.com>
2021-02-28 19:08:20 +0330

    Get account balance

--- programming-languages/web3/index.js
+++ programming-languages/web3/index.js
@@ -4,4 +4,5 @@ var url = 'https://1p7ENMTratsqhWuxVfnR63RhMZa:97dd35787a8e7aa649b9f6927f257ce4@
 
 var web3 = new Web3(url)
 
-console.log(web3,'hello world!')
+var address = '0xBE0eB53F46cd790Cd13851d5EFf43D12404d33E8'
+console.log(web3.fromWei(web3.eth.getBalance(web3.eth.address))) 
