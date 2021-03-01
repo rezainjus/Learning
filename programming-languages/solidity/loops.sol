f8b5e365c2914cd8a9eca258a7292702399c12a3
Reza Aghajani <rezaaghajani@protonmail.com>
2021-03-01 17:29:28 +0330

    Learn about Loops & Operators in Solidity ✔

---
+++ programming-languages/solidity/loops.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.6.8;
+
+contract Loops {
+    function division() public view returns(uint256) {
+        return 666/3;
+    }
+    
+    uint public num1 = 0;
+    function increment() public returns(uint) {
+        return num1++;
+    }
+    
+    
+}
